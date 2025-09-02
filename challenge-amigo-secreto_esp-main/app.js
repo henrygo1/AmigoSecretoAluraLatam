41621147ee3641b1c8cf28c9c133a8aaac53c3f3
henrygo1 <hg7451323@gmail.com>
2025-09-02 17:32:36 -0500

    Finalizado

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -4,8 +4,9 @@ let amigos = [];
 function agregarAmigo() {
   let amigo = document.getElementById('amigo').value;
 
-  if (amigo == '' || !/^[a-zA-Z\s]+$/.test(amigo)) {
+  if (amigo == '' || !/^[a-zA-Z\s]+$/.test(amigo) || amigo.length < 3) {
     alert('Por favor, inserte un nombre');
+    limpiarEntrada();
   } else {
     amigos.push(amigo);
     limpiarEntrada();
@@ -29,4 +30,17 @@ function listarAmigos(amigos = '') {
   }
 }
 
-
+function sortearAmigo(amigos="") {
+  if (amigos == '') {
+    alert('Aun no ha agregado ningun amigo');
+  } else {
+    let numeroGenerado = Math.floor(Math.random() * amigos.length);
+    console.log(numeroGenerado)
+    let amigoElegido = amigos[numeroGenerado];
+    const listaResultado = document.getElementById('resultado');
+    listaResultado.innerHTML="";
+    const li = document.createElement("li");
+    li.textContent = `El amigo elegido es ${amigoElegido}`;
+    listaResultado.appendChild(li);
+  }
+}
